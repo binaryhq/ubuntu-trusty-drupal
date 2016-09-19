d78ae1830d525b388ec7a1fd189bfb3f05a83872
ningappa <ningappa@users.noreply.github.com>
2016-09-19 19:19:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,25 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
 else
     echo "=> Using an existing volume of MySQL"
 fi
+
+RUN /etc/init.d/mysql start && \
+	cd /var/www/html && \
+	drupal site:install standard \
+		--langcode en \
+		--site-name="Drupal 8" \
+		--db-type='mysql' \
+		--db-host="localhost" \
+		--db-port=3306 \
+		--db-user=${MYSQL_USER:-'admin'} \
+		--db-pass=${MYSQL_PASS:-'admin'} \
+		--db-name=${MYSQL_DBNAME:-'admin'} \
+		--db-prefix="drupal_" \
+		--site-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
+		--account-name=${WP_USER:-'admin'} \
+		--account-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
+		--account-pass=${WP_PASS:-'password'}
+		
+		
 drupal check
 cd /var/www/html && \
 	drupal module:download admin_toolbar --latest && \ 
