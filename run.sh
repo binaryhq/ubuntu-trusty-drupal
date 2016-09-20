3ec9575668cecb9bfa945fce40a157223ba3ab7d
ningappa <ningappa@users.noreply.github.com>
2016-09-20 13:50:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -80,23 +80,21 @@ sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILE
 #    echo "=> Using an existing volume of MySQL"
 #fi
 exec supervisord -n
-
-
-	cd /var/www/html && \
-	drupal site:install standard \
-		--langcode en \
-		--site-name="Drupal 8" \
-		--db-type='mysql' \
-		--db-host="localhost" \
-		--db-port=3306 \
-		--db-user=${MYSQL_USER:-'admin'} \
-		--db-pass=${MYSQL_PASS:-'admin'} \
-		--db-name=${MYSQL_DBNAME:-'admin'} \
-		--db-prefix="drupal_" \
-		--site-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
-		--account-name=${WP_USER:-'admin'} \
-		--account-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
-		--account-pass=${WP_PASS:-'password'} 
+cd /var/www/html && \
+drupal site:install standard \
+	--langcode en \
+	--site-name="Drupal 8" \
+	--db-type='mysql' \
+	--db-host="localhost" \
+	--db-port=3306 \
+	--db-user=${MYSQL_USER:-'admin'} \
+	--db-pass=${MYSQL_PASS:-'admin'} \
+	--db-name=${MYSQL_DBNAME:-'admin'} \
+	--db-prefix="drupal_" \
+	--site-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
+	--account-name=${WP_USER:-'admin'} \
+	--account-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
+	--account-pass=${WP_PASS:-'password'} 
 
 drupal check && \
 	drupal module:download admin_toolbar --latest && \ 
